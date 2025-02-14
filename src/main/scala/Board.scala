cce96c876d72c5351a2f36c1074e5db7d2dc4682
Ashley George <ashgeo1986@gmail.com>
2025-02-14 17:02:19 +0000

    test file created

--- src/main/scala/Board.scala
+++ src/main/scala/Board.scala
@@ -5,4 +5,15 @@ case class Board (characters: List[Character]) {
   characterToGuess.name == name
   }
 
+  def askQuestion(userQuestion: String, guess: String = ""): Boolean = {
+    if (userQuestion == "male") {
+      characterToGuess.isMale
+    } else if (userQuestion == "female") {
+      !characterToGuess.isMale
+    } else if (userQuestion == "hair") {
+      characterToGuess.hairColour == guess
+    } else if (userQuestion == "eyes") {
+      characterToGuess.eyeColour == guess
+    } else false
+  }
 }
