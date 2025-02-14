b3f1f1dab3843d9ac6c72413a02a566a0947aebb
Nimo Mohamed <138133949+nimo-mohamed@users.noreply.github.com>
2025-02-14 15:11:21 +0000

    created some charecters

---
+++ src/main/scala/GameLogic.scala
@@ -0,0 +1,13 @@
+object GameLogic extends App {
+
+  val character1 = Character("Ash", isMale = true, "Green", "brown")
+  val character2 = Character("Nemo", isMale = false, "brown", "black")
+  val character3 = Character("Tudor", isMale = true, "brown", "black")
+  val character4 = Character("Lan", isMale = false, "Green", "black")
+  val character5 = Character("April", isMale = false, "red", "pink")
+
+
+  val board = Board(List(character1, character2))
+
+  println(board.guessCharecter("Lan"))
+}
