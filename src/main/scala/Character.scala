dcbdeffcd315967da8612a8ac32085c246709d01
tudorsonycx <tudorsonycx@gmail.com>
2025-02-17 18:08:06 +0000

    reorder parameters in Character case class for improved readability

--- src/main/scala/Character.scala
+++ src/main/scala/Character.scala
@@ -1,1 +1,1 @@
-case class Character(name: String, jumperColour: String, eyeColour: String, hairColour: String, isMale: Boolean,  hasGlasses: Boolean, hasBeard: Boolean, hasHat: Boolean, hasPet: Boolean)
+case class Character(name: String, eyeColour: String, hairColour: String, jumperColour: String, isMale: Boolean, hasGlasses: Boolean, hasBeard: Boolean, hasHat: Boolean, hasPet: Boolean)
