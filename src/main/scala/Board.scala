fb13e08745dc70736e231597e865a7a19b088818
tudorsonycx <tudorsonycx@gmail.com>
2025-02-17 15:32:25 +0000

    fix formatting in Board.scala

--- src/main/scala/Board.scala
+++ src/main/scala/Board.scala
@@ -3,6 +3,7 @@ import scala.util.Random
 case class Board(characters: List[Character]) {
 
   private val randomNumGen = new Random()
+
   private val randomIndex = randomNumGen.nextInt(characters.length)
 
   private val characterToGuess = characters(randomIndex)
@@ -20,15 +21,15 @@ case class Board(characters: List[Character]) {
       characterToGuess.hairColour == guess
     } else if (userQuestion == "eyes") {
       characterToGuess.eyeColour == guess
-    } else if (userQuestion == "jumper"){
+    } else if (userQuestion == "jumper") {
       characterToGuess.jumperColour == guess
-    } else if (userQuestion == "glasses"){
+    } else if (userQuestion == "glasses") {
       characterToGuess.hasGlasses
-    } else if (userQuestion == "pet"){
+    } else if (userQuestion == "pet") {
       characterToGuess.hasPet
-    } else if (userQuestion == "beard"){
+    } else if (userQuestion == "beard") {
       characterToGuess.hasBeard
-    } else if (userQuestion == "hat"){
+    } else if (userQuestion == "hat") {
       characterToGuess.hasHat
     } else false
   }
